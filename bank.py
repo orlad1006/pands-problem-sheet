e397ed24b0cd529cda8f5f163a4995d6ecf708dd
Wayne O'Brien <orlad1006@gmail.com>
2023-02-25 20:55:18 +0000

    week02 task bank.py

---
+++ bank.py
@@ -0,0 +1,13 @@
+# week2_task
+#bank.py
+#program to add two amounts of money
+#input reads in a string so we need to convert it to an integer
+#so we can then do mathematical interpretation
+# i need to research currency formats
+
+
+x = int(input('Enter amount 1: '))
+print (x)
+print(f'x' + str(1))
+
+
