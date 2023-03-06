1e978c8e09e98e97d7bcc54e55c66bc220528eae
Wayne O'Brien <orlad1006@gmail.com>
2023-03-06 20:15:43 +0000

    Week 3 task updated

--- accounts.py
+++ accounts.py
@@ -4,15 +4,28 @@
 # Write program that reads in a 10 character account number and outputs account number with only the last
 # 4 digits showing e.g. xxxxxx2589
 
+from dataclasses import replace
+
+
 accountnum = str(input("Please enter a 10 digit account number:  "))
 
 print("XXXXXX" + accountnum[6:])
 
 
 #modidy the program to  deal with account numbers of any lenght
-# use negative indexing to print last 4 digits only
+# print last 4 digits only
+# all characters to position -5 replace with X
 
-print(accountnum[-4:])
+initial_digits= (accountnum)[0:-5]
+last_4_digits = (accountnum)[-4:]   
+
+print(last_4_digits)
+
+
+
+
+  
+
 
 
 
