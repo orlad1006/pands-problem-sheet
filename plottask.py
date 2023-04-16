4a8d56efdc0a3b2b8768f0f1d1c5ddd02424acd4
Wayne O'Brien <orlad1006@gmail.com>
2023-04-16 12:22:21 +0100

    week_8_task

---
+++ plottask.py
@@ -0,0 +1,11 @@
+#Title: plottask.py
+#Author: Orla Dowling
+#Task: Write a program called plottask.py that displays:
+#a histogram of a normal distribution of a 1000 values with a mean of 5 and standard deviation of 2, 
+#and a plot of the function  h(x)=x3 in the range [0, 10], 
+#on the one set of axes.
+#Some marks will be given for making the plot look nice (legend etc).
+
+import numpy as np # 
+import matplotlib.pyplot as plt
+
